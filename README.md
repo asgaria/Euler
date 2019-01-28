9365f1687e9fb7c3aed3f86a8054a0efe0fd7b50
asgaria <asgaria@oregonstate.edu>
2019-01-27 20:00:56 -0800

    Completed problem 1

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Euler
+Problem 1: Accidentally misread problem statement and thought it asked to INCLUDE 1000. 
--- problem1.py
+++ problem1.py
@@ -1,1 +1,16 @@
-print("HI");
+def isMultiple(num):
+	if(num % 3 == 0):
+		return True;
+	elif(num % 5 == 0):
+		return True;
+	else:
+		return False
+
+sumNum = 0;
+
+for i in range(1, 1000):
+	if isMultiple(i):
+		sumNum += i;
+	
+
+print sumNum;
